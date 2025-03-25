dab586cbb466e63d3cba11cabe11eed28a770f3c
Dinaest2024 <estefanyenriquez97@gmail.com>
2025-03-25 13:14:56 -0500

    test

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: false,
+})
+export class Tab1Page {
+
+  constructor(public fotoService: FotoService) {}
+
+  addPhotoToGallery()
+  {
+    this.fotoService.addNewToGallery()
+  }
+
+}
